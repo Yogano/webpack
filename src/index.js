5b983e319186e39a0c0d5b06d9dedcd1876a3ac5
Yogano <38916107+Yogano@users.noreply.github.com>
2020-09-22 02:36:00 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import Post from '@models/Post';
+// import json from './assets/json';
+// import xml from './assets/data.xml';
+// import csv from './assets/data.csv';
+import WebpackLogo from '@/assets/webpack-logo';
+import './styles/styles.css';
+
+const post = new Post('Webpack Post Title', WebpackLogo);
+
+console.log('Post to string', post.toString());
+
+// console.log('JSON:', json);
+// console.log('XML:', xml);
+// console.log('CSV:', csv);
